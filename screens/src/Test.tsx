bbfbe0d58da84dabf07eaa8daf74faf2de27cf1f
GS-755 <21dh114236@st.huflit.edu.vn>
2023-12-19 02:07:56 +0700

    Rollback with removing MainLayout

--- screens/src/Test.tsx
+++ screens/src/Test.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
 import Styles from "../../style/Styles";
-import MainLayout from "../layout/MainLayout";
 
 const Test = (): JSX.Element => {
   return (
     <>
-      <MainLayout>
-        <View style={Styles.container}>
-          <Text className="text-3xl">Test</Text>
-        </View>
-      </MainLayout>
+      <View style={Styles.container}>
+        <Text className="text-3xl">Test</Text>
+      </View>
     </>
   );
 }
